5aa0dfb841867f2f3604b57d483ffb38df169f61
wsmith <02whitten.smith@gulllakecs.org>
2022-05-13 09:45:05 -0700

    Fixed accidental deletion

---
+++ script.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+
+#set color variables for menus and interface
+BLUE='\033[0;34m'
+RED='\033[0;031m'
+GREEN='\033[0;032m'
+NC='\033[0m'
+
+
+function HomeScreen(){
+
+	cd ~
+	sudo unalias -a
+
+	#main screen display
+	echo -e "${RED}********************************************************************************"
+	echo -e "********************************************************************************${NC}"
+	echo -e "${RED}*${NC}                          (#%%%%%%%%%%%%# &                                   ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %%${BLUE}%%%%%%%%%%%%%%${NC}%#                                   ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%%${NC}%%%%%%${BLUE}%%%%%${NC}#                                   ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&    (#%%%%#                                   ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&                                              ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&  /###########                                ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&  /#${BLUE}%%%%%%%%${NC}%%                                ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&  /#${BLUE}%%%%%%%%${NC}%%                                ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%${NC}#&     ((${BLUE}%%%%${NC}%(                                 ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%%${NC}%%%%%%%${BLUE}#%%%%${NC}%(                                 ${RED}*${NC}"
+	echo -e "${RED}*${NC}                         %${BLUE}%%%%%%%%%%%%%%%%%${NC}%(                                 ${RED}*${NC}"
+	echo -e "${RED}*${NC}                          (#%%%%%%%%%%${BLUE}%%%%%${NC}%(                                 ${RED}*${NC}"
+	echo -e "${RED}*${NC}                                     ((${BLUE}%%%%${NC}%(  #((((((                        ${RED}*${NC}"
+	echo -e "${RED}*${NC}                                     ((${BLUE}%%%%${NC}%(  (#${BLUE}%%%${NC}%(                        ${RED}*${NC}"
+	echo -e "${RED}*${NC}                                     %#${BLUE}%%%%%%%%%%%%%${NC}%(                        ${RED}*${NC}"
+	echo -e "${RED}*${NC}                                     %%%%%%%%%%%%%%%%(                        ${RED}*${NC}"
+	echo -e "${RED}*${NC}//////////////////////WELCOME TO ${RED}HYDRATECH'S${NC} CYBERSCRIPT//////////////////////${RED}*${NC}"
+	echo -e "${RED}*${NC}                                                                              ${RED}*${NC}"
+	echo -e "${RED}*${NC}///////////////////////////////ENTER ${GREEN}1${NC} TO BEGIN///////////////////////////////${RED}*${NC}"
+	echo -e "${RED}********************************************************************************"
+	echo -e "********************************************************************************${NC}"
+
+	homeAns=" "
+
+	read homeAns
+
+	case $homeAns in
+
+		1 )
+			#sudo apt-get update
+
+			MainScreen
+			;;
+
+		* )
+			HomeScreen
+			;;
+
+	esac
+}
